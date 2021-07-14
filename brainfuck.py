7c241eeedb6aef665704320065bdf18ebc9a4ff6
Michael Richards <fluffy.trooper.2001@gmail.com>
2021-07-14 10:47:36 +1000

    make brainfuck better

--- brainfuck.py
+++ brainfuck.py
@@ -1,5 +1,4 @@
 """
-Brainfuck String Processing
 + increment
 - decrement
 < move one cell left
@@ -8,132 +7,51 @@ Brainfuck String Processing
 . push cell value to output as character.
 [ while cell != 0 loop code between this character and matching ]
 ] if cell = 0, exit loop, else go back to matching [
-! separate code and input
 """
+chars = '+-<>.,[]'
+class BrainfuckError(RuntimeError):...
 
-def brainfuck(codeinput):
-    if codeinput == '[':
-        return "```bf\nInvalid Syntax: The whole code is just '[', you fool\n```"
-    code = codeinput.split(sep='!')[0]
-    after_code = 0; user_input = [0]
-    for char in codeinput:
-        if not after_code:
-            if char == '!':
-                after_code = 1
+def brainfuck(code,input=input):
+    from collections import defaultdict as d
+    from ctypes import py_object as p
+    hack=lambda v:p.from_address(v)
+    l=input==__builtins__.input
+    if not l:input=lambda i=1:input.pop(i-1)
+    a,o=d(int),''
+    i=p=t=0
+    gulag = [hack(id(IndexError)).value]
+    hack(id(__builtins__.IndexError)).value=StopIteration
+    for c in iter(lambda:code[p],None):
+        def __0(i,*_):
+            a[i]+=1
+            a[i]%=256
+            return i,*_,
+        def __1(i,*_):
+            a[i]-=1
+            a[i]%=256
+            return i,*_,
+        def __2(i,o,t):
+            o+=chr(a[i])
+            return i,o,t
+        def __3(i,*_):
+            a[i]+=ord(input(1))%256
+            return i,*_,
+        f = [__0,__1,
+             lambda i,*_:(i+1,*_),
+             lambda i,*_:(i-1,*_),
+             __2,__3,
+             lambda i,o,t:(i,o,t+(not a[i])),
+             lambda i,o,t:(i,o,t-bool(a[i])),lambda*_:_][chars.find(c)]
+        if t:
+            if c=='[':t+=1
+            if c==']':t-=1
         else:
-            if user_input[0]:
-                user_input.append(ord(codeinput[0]))
-            else:
-                user_input = [ord(codeinput[0])]
-        codeinput = codeinput[1:]
-    if user_input == None:
-        user_input = [0]
-    # code and input are now split. Now, compile the code with input.
-    class Data:
-        cell_memory = [0]
-        cell_pointer = 0
-        input = user_input
-    #def compile
-    def bfcompile(code, Data):
-        if len(code) > 0:
-            if code[0] == ']':
-                return "```bf\nInvalid Syntax: Started with end_of_loop ']'\n```", Data
-            elif len(code) != 1:
-                if code[-1] == '[':
-                    return "```bf\nInvalid Syntax: Ended with begin loop '['\n```", Data
-            if code == '[':
-                return "```bf\nInvalid Syntax: Entire code is just '[', you fool\n```"
-            num_left = len(code); output = [0]
-            while num_left:
-                this = code[0]; print(this)
-                if len(code) != 1:
-                    code = code[1:]
-                else:
-                    code = [0]
-                    num_left = 0
-                if this == '+':
-                    Data.cell_memory[Data.cell_pointer] += 1
-                elif this == '-':
-                    Data.cell_memory[Data.cell_pointer] -= 1
-                elif this == '<':
-                    if Data.cell_pointer == 0:
-                        temp = Data.cell_memory; Data.cell_memory = [0]
-                        for cell in temp:
-                            Data.cell_memory.append(cell)
-                    else:
-                        Data.cell_pointer -= 1
-                elif this == '>':
-                    if Data.cell_pointer == len(Data.cell_memory)-1:
-                        Data.cell_memory.append(0)
-                    Data.cell_pointer += 1
-                elif this == ',':
-                    Data.cell_memory[Data.cell_pointer] = Data.input[0]
-                    if Data.input != [0]:
-                        if len(Data.input) == 1:
-                            Data.input = [0]
-                        else:
-                            Data.input = Data.input[1:]
-                elif this == '.':
-                    if output[0]:
-                        output.append(Data.cell_memory[Data.cell_pointer])
-                    else:
-                        output = [Data.cell_memory[Data.cell_pointer]]
-                elif this == '[': # loop
-                    nest_count = 1; temp_code = ''
-                    while nest_count:
-                        if len(code)-1:
-                            this = code[0]
-                            code = code[1:]
-                            temp_code = temp_code + this
-                            if this == '[':
-                                nest_count += 1
-                            elif this == ']':
-                                nest_count -= 1
-                        elif code != ']':
-                            return "```bf\nInvalid Syntax: mismatched '[' detected\n```", Data
-                        else: # must be ']' and last char
-                            nest_count = 0
-                    if len(temp_code) > 1:
-                        if temp_code[-1] == ']':
-                            temp_code = temp_code[:-1]
-                            while len(temp_code) > 1 and temp_code[-1] == ']':
-                                temp_code = temp_code[:-1]
-                    if temp_code == ']':
-                        temp_code = ''
-                    if temp_code != '':
-                        while Data.cell_memory[Data.cell_pointer] != 0:
-                            temp_output, Data = bfcompile(temp_code,Data)
-                            for char in temp_output:
-                                if str(char) != char:
-                                    output.append(char)
-                                else:
-                                    output.append(ord(char))
-                    if code == ']':
-                        code = ' '
-                elif this == ']':
-                    if output == '' or output == [0]:
-                        output = list("```bf\nInvalid Syntax: mismatched '[' detected\n```")
-                    return output, Data
-            return output, Data
-        else:
-            return [0], Data
-    # finish defining and run
-    rare_output, _ = bfcompile(code,Data)
-    output = ''
-    for char in rare_output:
-        if char != 0 and char != str(char):
-            if chr(char) == '\b' and len(output) > 1:
-                output = output[:-1]
-            elif char == 127:
-                pass
-            else:
-                output = output + chr(char)
-        elif char == str(char):
-            output = output + char
-    return output
+            i,o,t = f(i,o,t)
+        p += 1 - 2*(t<0)
+    hack(id(IndexError)).value = gulag.pop(0)
+    return o,int(p!=len(code))
 
 try:
     print(brainfuck(input("bf> ")))
-except:
-    BrainfuckError = RuntimeError
-    raise BrainfuckError("ERROR IN THE BRAINFUCKING")
+except Exception as e:
+    raise BrainfuckError(e)
